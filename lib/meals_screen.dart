0631cc7332c8f5182eef42b963831a0a90b0a14a
yatharth25 <54071856+yatharth25@users.noreply.github.com>
2021-01-20 20:56:53 +0530

    basic app working

---
+++ lib/meals_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'dummy_data.dart';
+
+class MealsScreen extends StatelessWidget {
+  static const routeName = '/category';
+  @override
+  Widget build(BuildContext context) {
+    final routeArgs =
+        ModalRoute.of(context).settings.arguments as Map<String, String>;
+    final categoryTitle = routeArgs['title'];
+    final categoryId = routeArgs['id'];
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(categoryId);
+    }).toList();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(categoryTitle),
+      ),
+      body: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return Text(categoryMeals[index].title);
+        },
+        itemCount: categoryMeals.length,
+      ),
+    );
+  }
+}
